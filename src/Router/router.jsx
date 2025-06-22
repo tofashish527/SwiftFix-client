251f12666dcd5954e1a53cae1f91eae918cccd60
tofashish527 <tofashish000@gmail.com>
2025-06-22 23:13:34 +0600

    project setup

---
+++ src/Router/router.jsx
@@ -0,0 +1,13 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+export default router;
